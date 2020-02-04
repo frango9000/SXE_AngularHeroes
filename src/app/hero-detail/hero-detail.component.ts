957a68b666e27c6b0380baea6513d972d84514cf
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-02-04 17:11:19 +0100

    Routing https://angular.io/tutorial/toh-pt5
    Summary
    You added the Angular router to navigate among different components.
    You turned the AppComponent into a navigation shell with <a> links and a <router-outlet>.
    You configured the router in an AppRoutingModule
    You defined simple routes, a redirect route, and a parameterized route.
    You used the routerLink directive in anchor elements.
    You refactored a tightly-coupled master/detail view into a routed detail view.
    You used router link parameters to navigate to the detail view of a user-selected hero.
    You shared the HeroService among multiple components.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from "../Hero";
 
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+
+import {HeroService} from '../hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -8,12 +13,24 @@ import {Hero} from "../Hero";
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero;
+  hero: Hero;
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+              private heroService: HeroService,
+              private location: Location) {
   }
 
   ngOnInit() {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
